d46ac0cd31732cb09c0d697e76461f58e24bc81f
MSanan791 <sanan791@gmail.com>
2024-08-29 13:20:45 +0500

    cors everywhere

--- src/components/content/content.jsx
+++ src/components/content/content.jsx
@@ -11,7 +11,7 @@ export default function Content(){
 
   async function getEmployees() {
     try {
-      const response = await axios.get('http://23.22.252.76:9000/users/employees');
+      const response = await axios.get('https://cors-anywhere.herokuapp.com/http://23.22.252.76:9000/users/employees');
       console.log(response);
       setTableData(response.data); // Use response.data to access the data
     } catch (error) {
